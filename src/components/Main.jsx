e281de4a6c74243f14b10c38a9364882985e7c11
Ren Yamazaki <r26763041@gmail.com>
2025-06-23 06:02:52 +0900

    Markdownを使用できるように変更

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Main.css";
+import ReactMarkdown from "react-markdown";
 
 const Main = ({ activeNote, onUpdateNote }) => {
   const onEditNote = (key, value) => {
@@ -20,19 +21,23 @@ const Main = ({ activeNote, onUpdateNote }) => {
         <input
           id="title"
           type="text"
-          value={activeNote.title}
+          value={activeNote.title || ""}
           onChange={(e) => onEditNote("title", e.target.value)}
         />
         <textarea
           id="content"
           placeholder="ノート内容を記入"
-          value={activeNote.content}
+          value={activeNote.content || ""}
           onChange={(e) => onEditNote("content", e.target.value)}
         ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
-        <div className="markdown-preview">{activeNote.content}</div>
+        <div className="markdown-preview">
+          <ReactMarkdown>
+            {activeNote.content || "# 新しいノート\n\n内容を入力してください"}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   );
